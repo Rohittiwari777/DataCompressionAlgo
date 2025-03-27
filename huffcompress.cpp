d99ef7d71c12abb51ff1c57325d97b4ce4dc1e28
Ankit  Adhikari <ankitadhikari@Ankits-MacBook-Air.local>
2025-03-27 11:45:31 +0530

    code edited

--- huffcompress.cpp
+++ huffcompress.cpp
@@ -3,6 +3,10 @@
 #include <string.h>
 #include <math.h>
 #include <unordered_map>
+#include <fstream>
+#include<bits/stdc++.h>
+
+
 using namespace std;
 
 #define io                        \
@@ -29,18 +33,8 @@ typedef struct Minheap *HEAP;
 
 NODE newleafnode(char c, int f)
 {
-    NODE tmp = (NODE)malloc(sizeof(struct huffnode));
+    NODE tmp = new huffnode();
     tmp->character = c;
-    tmp->freq = f;
-    tmp->left = NULL;
-    tmp->right = NULL;
-    return tmp;
-}
-
-NODE newinternalnode(int f)
-{
-    NODE tmp = (NODE)malloc(sizeof(struct huffnode));
-    tmp->character = 0;
     tmp->freq = f;
     tmp->left = NULL;
     tmp->right = NULL;
@@ -49,33 +43,25 @@ NODE newinternalnode(int f)
 
 void swap(NODE *a, int i, int j)
 {
-    NODE tmp = (NODE)malloc(sizeof(huffnode));
-    tmp = a[i];
+    NODE tmp = a[i];
     a[i] = a[j];
     a[j] = tmp;
 }
 
 void heapify(HEAP heap)
 {
-    int i, j, k;
     unsigned short n = heap->size;
     NODE *a = heap->array;
-    for (i = n / 2; i > 0; i--)
+    for (int i = n / 2; i > 0; i--)
     {
-        if (2 * i + 1 <= n)
-            j = a[2 * i]->freq < a[2 * i + 1]->freq ? 2 * i : 2 * i + 1;
-        else
-            j = 2 * i;
-
+        int j = (2 * i + 1 <= n) ? ((a[2 * i]->freq < a[2 * i + 1]->freq) ? 2 * i : 2 * i + 1) : (2 * i);
         if (a[j]->freq < a[i]->freq)
         {
             swap(a, i, j);
+            int k;
             while (j <= n / 2 && (a[j]->freq > a[2 * j]->freq || (2 * j + 1 <= n ? a[j]->freq > a[2 * j + 1]->freq : 0)))
             {
-                if (2 * j + 1 <= n)
-                    k = a[2 * j]->freq < a[2 * j + 1]->freq ? 2 * j : 2 * j + 1;
-                else
-                    k = 2 * j;
+                k = (2 * j + 1 <= n) ? ((a[2 * j]->freq < a[2 * j + 1]->freq) ? 2 * j : 2 * j + 1) : (2 * j);
                 swap(a, k, j);
                 j = k;
             }
@@ -85,8 +71,7 @@ void heapify(HEAP heap)
 
 NODE mintop(HEAP heap)
 {
-    NODE top = (NODE)malloc(sizeof(huffnode));
-    top = heap->array[1];
+    NODE top = heap->array[1];
     heap->array[1] = heap->array[heap->size--];
     heapify(heap);
     return top;
@@ -94,7 +79,7 @@ NODE mintop(HEAP heap)
 
 void insertnode(HEAP heap, NODE leftchild, NODE rightchild, int f)
 {
-    NODE tmp = (NODE)malloc(sizeof(struct huffnode));
+    NODE tmp = new huffnode();
     tmp->character = '\0';
     tmp->freq = f;
     tmp->left = leftchild;
@@ -115,177 +100,106 @@ void huffman_tree(HEAP heap)
 
 void assign_codes(NODE root, string str)
 {
-    string s1 = "", s2 = "";
     if (!root->left && !root->right)
     {
         hashmap[root->character] = str;
     }
     else
     {
-        s1 = str + "0";
-        s2 = str + "1";
-        assign_codes(root->left, s1);
-        assign_codes(root->right, s2);
+        assign_codes(root->left, str + "0");
+        assign_codes(root->right, str + "1");
     }
 }
 
-void preorder(NODE root)
+int main()
 {
-    if (root != NULL)
-    {
-        cout << root->freq << "\t";
-        preorder(root->left);
-        preorder(root->right);
-    }
-}
+    io;
 
-int main(int argc, char *argv[])
-{
-    io if (argc != 2)
+    string input_filename = "input.txt"; // File to be compressed
+    string output_filename = "compressed.bin";
+
+    ifstream input(input_filename, ios::binary);
+    if (!input)
     {
-        cout << "Invalid cmd line arg. Usage: ./a.out <input file>\n";
+        cout << "Error: Cannot open " << input_filename << endl;
         return 1;
     }
-    FILE *input = fopen(argv[1], "r");
-    if (input == NULL)
+
+    ofstream output(output_filename, ios::binary);
+    if (!output)
     {
-        cout << "Error opening input file\n";
+        cout << "Error: Cannot create " << output_filename << endl;
         return 2;
     }
-    string in = argv[1];
-    string filetype = in.substr(in.find(".")+1);
-    cout<<filetype<<"\n";
-    string out = in.substr(0, in.find(".")) + "-compressed.bin";
 
-    FILE *output = fopen(out.c_str(), "wb");
-    if (output == NULL)
+    // Read file content and calculate frequency
+    int freq[256] = {0};
+    char ch;
+    int file_size = 0, num_of_unique_chars = 0;
+    while (input.get(ch))
     {
-        cout << "Error creating output file\n";
-        return 3;
-    }
-    string file_contents = "";
-    char buff[1];
-    unsigned short num_of_unique_chars = 0;
-
-    // Write file type
-    int ftsize = filetype.length();
-    char num = ftsize + '0';
-    fwrite(&num, 1, 1, output);
-    for(int i=0; i<ftsize; i++){
-        buff[0] = filetype[i];
-        fwrite(buff, 1, 1, output);
-    }
-
-    // Holds frequency of each unique character
-    int i, f_size = 0, freq[256] = {0};
-    while (fread(buff, 1, 1, input))
-    {
-        if (!freq[buff[0] - 0])
+        if (!freq[(unsigned char)ch])
             num_of_unique_chars++;
-        freq[buff[0] - 0]++;
-        f_size++;
+        freq[(unsigned char)ch]++;
+        file_size++;
     }
-    fclose(input);
-    FILE *input1 = fopen(argv[1], "r");
 
-    HEAP heap = (HEAP)malloc(sizeof(struct Minheap));
+    input.clear();
+    input.seekg(0, ios::beg);
 
-    // Capacity holds (number of unique characters + 1)
-    //cout << "\n\nNUMBER OF UNIQUE CHARS - " << num_of_unique_chars << "\n\n";
+    // Create a min-heap
+    HEAP heap = new Minheap();
     heap->capacity = num_of_unique_chars + 1;
     heap->size = 0;
-    heap->array = (NODE *)malloc(heap->capacity * sizeof(struct huffnode));
+    heap->array = new NODE[heap->capacity];
 
     for (int i = 0; i < 256; i++)
     {
         if (freq[i])
         {
-            heap->size += 1;
-            heap->array[heap->size] = newleafnode(i, freq[i]);
+            heap->array[++heap->size] = newleafnode(i, freq[i]);
         }
-    }
-
-    for (int i = 1; i <= heap->size; i++)
-    {
-        //cout<<heap->array[i]->character<<" - "<<heap->array[i]->freq<<"\n";
     }
 
     heapify(heap);
-    //cout << "Heapify done\n";
-    for (int i = 1; i <= heap->size; i++)
-    {
-        //cout<<heap->array[i]->character<<" - "<<heap->array[i]->freq<<"\n";
-    }
     huffman_tree(heap);
     assign_codes(heap->array[heap->size], "");
-    unordered_map<char, string>::iterator x;
 
-    //cout << "\nCodes\n";
-    for (x = hashmap.begin(); x != hashmap.end(); x++)
-        //cout<<x->first<<" - "<<x->second<<'\n';
-
-        // Convert file content to binary code
-        // By assigning codes to each character
-        while (fread(buff, 1, 1, input))
-        {
-            file_contents += hashmap[buff[0]];
-        }
-    //cout<<file_contents.length()<<"\n"<<file_contents<<"\n";
-    // Find the padding given to make binary code length a multiple of 8
-    int padding = 0;
-    if (file_contents.length() % 8 != 0)
-        padding = 8 - (file_contents.length() % 8);
-    for (int i = 0; i < padding; i++)
+    // Encode file content into binary string
+    string file_contents = "";
+    while (input.get(ch))
     {
-        file_contents = "0" + file_contents;
+        file_contents += hashmap[ch];
+    }
+    input.close();
+
+    // Add padding to make it multiple of 8
+    int padding = (8 - file_contents.length() % 8) % 8;
+    file_contents.insert(0, padding, '0');
+
+    // Write Huffman codes to output file
+    for (auto &pair : hashmap)
+    {
+        output.put(pair.first);
+        output << pair.second << '\n';
+    }
+    output.put('\0'); // Separator
+
+    // Write padding info
+    output.put(padding + '0');
+
+    // Convert binary string to bytes and write to file
+    for (size_t i = 0; i < file_contents.size(); i += 8)
+    {
+        bitset<8> bits(file_contents.substr(i, 8));
+        output.put(static_cast<char>(bits.to_ulong()));
     }
 
-    // Write the codes assigned to characters at the start of the file
-    string codes;
-    unordered_map<char, string>::iterator iter;
-    for (iter = hashmap.begin(); iter != hashmap.end(); iter++)
-    {
-        codes = "";
-        codes += (iter->first + iter->second);
-        fwrite(codes.c_str(), codes.length() + 1, 1, output);
-        //cout<<iter->first<<"\t"<<iter->second<<"\n";
-    }
+    cout << "Compression successful! Compressed file: " << output_filename << endl;
+    output.close();
 
-    // Write the padding given to the binary code
-    char null = '\0';
-    char pad = padding + '0';
-    fwrite(&null, 1, 1, output);
-    fwrite(&pad, 1, 1, output);
-    fwrite(&null, 1, 1, output);
-    fwrite(&null, 1, 1, output);
-    // To identify padding with 2 null chars both sides
+    delete[] heap->array;
+    delete heap;
 
-    // Take 8 bit binary code -> decimal -> character
-    // And write this character to ouput file.
-    char tmp_bin[9];
-    int dec, w;
-    //cout<<"\n";
-    for (int j = 0; file_contents[j]; j = j + 8)
-    {
-        strncpy(tmp_bin, &file_contents[j], 8);
-        // Convert to integer equivalent and then character
-        dec = 0, i = 7;
-        while (i > -1)
-        {
-            w = tmp_bin[i] == '1' ? 1 : 0;
-            dec += ((int)pow(2, 7 - i)) * w;
-            i--;
-        }
-        // Print the binary code, its dec equivalent and finally corresponding ascii character.
-        //printf("%s\t%d\t%c\n", tmp_bin, dec, dec);
-        buff[0] = dec;
-        fwrite(buff, 1, 1, output);
-    }
-    cout<<"Compressed File successfully\n";
-    //cout << "NUMBER OF UNIQUE CHARS - " << num_of_unique_chars;
-    free(heap);
-    fclose(input1);
-    fclose(output);
-    //cout << "\nEND\n";
     return 0;
 }
--- huffdecompress.cpp
+++ huffdecompress.cpp
@@ -1,103 +1,73 @@
-#include<iostream> 
-#include<unordered_map> 
+#include <iostream>
+#include <unordered_map>
+#include <fstream>
+#include <bitset>
+#include <string>
+
 using namespace std;
 
-string dectobin(int decimal){
-    string s="";
-    for(int i = 7; i>=0; i--){
-        if(decimal>>i & 1)s += '1';
-        else s += '0';
-    }
-    return s;
+string dectobin(unsigned char decimal) {
+    return bitset<8>(decimal).to_string();
 }
 
-int main(int argc, char *argv[]){
-    if(argc != 2){
-        cout<<"Invalid cmd line arg. Usage: ./a.out <input file>\n";
+int main() {
+    string input_filename = "compressed.bin";  // Input compressed file
+    ifstream input(input_filename, ios::binary);
+    
+    if (!input) {
+        cerr << "Error: Cannot open " << input_filename << endl;
         return 1;
     }
-    FILE *input = fopen(argv[1], "rb");
-    int num_of_unique_chars=0;
-    if(input == NULL){
-        cout<<"Error opening input file\n";
+    
+    string output_filename = "decompressed_output.txt";
+    ofstream output(output_filename, ios::binary);
+    
+    if (!output) {
+        cerr << "Error: Cannot create " << output_filename << endl;
         return 2;
     }
-    string in = argv[1];
-    string out = in.substr(0,in.find("-compressed")) + "-decompressed.";
-    char buff[1];
-
-    // Read file extension length and the extension itself
-    fread(buff, 1, 1, input);
-    int fsize = buff[0] - '0';
-    while(fsize--){
-        fread(buff, 1, 1, input);
-        out += buff[0];
+    
+    unordered_map<string, char> decode_map;
+    
+    // Read Huffman code mappings
+    string line;
+    char c;
+    while (input.get(c)) {
+        if (c == '\0') break; // End of code section
+        
+        char key = c;
+        getline(input, line); // Read the code (until newline)
+        decode_map[line] = key;
     }
     
-    FILE *output = fopen(out.c_str(), "w");
-    if(output == NULL){
-        cout<<"Error creating output file\n";
-        return 3;
+    // Read padding information
+    char padding_char;
+    input.get(padding_char);
+    int padding = padding_char - '0';
+    
+    // Read binary data
+    string binary_str;
+    char byte;
+    while (input.get(byte)) {
+        binary_str += dectobin(static_cast<unsigned char>(byte));
     }
-    unordered_map<string, char> decode_map;
-    string s = "";
-    int flag=0;
-
-    // Read and fill the hashmap to decode later
-    while(fread(buff, 1, 1, input)){
-        if(buff[0] != '\0'){
-            flag = 0;
-            s += buff[0];
-        }
-        else{
-            num_of_unique_chars++;
-            flag++;
-            if(flag == 2)break;
-            decode_map[s.substr(1, s.length()-1)] = s[0];
-            s = "";
-        }
-    }
-
-    // Read and store the padding, read extra null chars 
-    fread(buff, 1, 1, input);
-    int padding = buff[0] - '0';
-    fread(buff, 1, 1, input);  // null
-    fread(buff, 1, 1, input);  // null
-
-    // Actual binary code begins from here
-    int decimal;
-    s = "";
-    while(fread(buff, 1, 1, input)){
-        // Get decimal(ASCII) equivalent of character
-        decimal = buff[0];
-        // Append the binary equivalent to s
-        s = s + dectobin(decimal);
-    }
-
-    // Ignore padding and
-    // Convert the binary code back to original text file.
-    int start;    
-    if(padding == 0){
-        s = "0" + s;
-        start = 1;
-    }
-    else start = padding;
-    for(int i=1; s[start]!='\0'; i++){
-        if(decode_map.find(s.substr(start, i)) != decode_map.end()){
-            fwrite(&(decode_map[s.substr(start, i)]), 1, 1, output);
-            //cout<<decode_map[s.substr(start, i)];
-            start = start + i;
-            i = 0;
+    
+    // Remove padding bits
+    binary_str = binary_str.substr(padding);
+    
+    // Decode binary string
+    string current_code;
+    for (char bit : binary_str) {
+        current_code += bit;
+        if (decode_map.find(current_code) != decode_map.end()) {
+            output.put(decode_map[current_code]);
+            current_code.clear();
         }
     }
-    cout<<"Decompressed File Succesfully.\n";
-
-    unordered_map<string, char>:: iterator iter;
-    for(iter = decode_map.begin(); iter!=decode_map.end(); iter++){
-        //cout<<iter->first<<"\t"<<iter->second<<"\n";
-    }
-    //cout<<"NUMBER OF UNIQUE CHARS - "<<num_of_unique_chars;
-    fclose(input);
-    fclose(output);
+    
+    cout << "Decompression successful! Output file: " << output_filename << endl;
+    
+    input.close();
+    output.close();
     return 0;
 }
