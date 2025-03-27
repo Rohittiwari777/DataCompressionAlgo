b56b615e541af6dd3e331ed3964a6d0e1586728c
Rohittiwari777 <146427628+Rohittiwari777@users.noreply.github.com>
2025-03-27 09:54:12 +0530

    Update huffcompress.cpp

--- huffcompress.cpp
+++ huffcompress.cpp
@@ -4,7 +4,7 @@
 #include <math.h>
 #include <unordered_map>
 using namespace std;
-
+/*hoffman compression*/
 #define io                        \
     ios_base::sync_with_stdio(0); \
     cin.tie(0);                   \
